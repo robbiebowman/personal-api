bf41cf959066f1abfe6151c7ad753c0fab84ae80
Robbie Bowman <robbiebowman@gmail.com>
2026-01-10 10:23:26 -0500

    Fix title game

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 	implementation("com.robbiebowman:claude-sdk:0.0.4")
 	implementation("com.github.robbiebowman:mini-crossword-maker:1.1.3")
 	implementation("com.github.robbiebowman:periodic-table-describer:0.0.3")
-	implementation("com.github.robbiebowman:title-game:1.0.6")
+	implementation("com.github.robbiebowman:title-game:1.0.7")
 
 	implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.2.0")
 
